0e61d4d56d047953ac92fea2f9f3debd720a187c
Rik de Goede <rik.dgd@gmail.com>
2024-09-15 23:58:10 +0200

    finished config constructor, add path validity checking.

--- src/config.rs
+++ src/config.rs
@@ -15,27 +15,22 @@ impl Config {
         match args.len() {
             3 => {
                 let path = &args[1];
+                let encryption_key = args[2].clone();
 
                 // If the path leads to a directory
                 if fs::read_dir(path).is_ok() { 
-                    let read = fs::read_dir(path)?;
-                    let mut files = Vec::new();
-                    for dir_entry in read.flatten() {
-                        let path = dir_entry
-                            .path()
-                            .to_str()
-                            .unwrap()
-                            .to_string();
-                        files.push(path);
-                    }
-                    
-                // If the path leads to a file    
-                } else if fs::read(path).is_ok() { 
-                    let paths= vec![path.to_string()];
-                    
+                    let paths = get_files_in_dir(path)?;
                     return Ok(Self {
                         paths,
-                        encryption_key: args[2].clone(),
+                        encryption_key,
+                    });
+                    
+                // If the path leads to a file    
+                } else if fs::read(path).is_ok() {
+                    let paths= vec![path.to_string()];
+                    return Ok(Self {
+                        paths,
+                        encryption_key,
                     });
                 }
 
@@ -52,4 +47,23 @@ impl Config {
             }
         }
     }
+}
+
+fn get_files_in_dir(dir_path: &str) -> IoResult<Vec<String>> {
+    let read = fs::read_dir(dir_path)?;
+    let mut files = Vec::new();
+    for dir_entry in read.flatten() {
+        let path = dir_entry
+            .path()
+            .to_str()
+            .unwrap()
+            .to_string();
+        
+        // Only add files inside the directory, not subdirectories.
+        if fs::read_dir(&path).is_err() {
+            files.push(path);
+        }
+    }
+    
+    Ok(files)
 }
